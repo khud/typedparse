9dbc9bd76efdcb5a85e37d3a25d78d79805ee501
khud <vitaly.khudobakhshov@gmail.com>
2020-12-31 15:43:10 +0300

    Bump version to 0.2.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="typedparse",
-    version="0.2",
+    version="0.2.1",
     author="Vitaly Khudobakhshov",
     author_email="vitaly.khudobakhshov@gmail.com",
     packages=["typedparse"],
